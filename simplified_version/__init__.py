6674163e168859f6e6d3b1f5fd2b99d227a70b35
Rodrigo Gambra <regm_98@yahoo.com>
2019-07-25 12:13:48 -0400

    Create __init__.py

---
+++ simplified_version/__init__.py
@@ -0,0 +1,19 @@
+# from App.libs.geojson.codec import dump, dumps, load, loads, GeoJSONEncoder
+# from App.libs.geojson.utils import coords, map_coords
+# from App.libs.geojson.geometry import Point, LineString, Polygon
+# from App.libs.geojson.geometry import MultiLineString, MultiPoint, MultiPolygon
+# from App.libs.geojson.geometry import GeometryCollection
+# from App.libs.geojson.feature import Feature, FeatureCollection
+# from App.libs.geojson.base import GeoJSON
+# from App.libs.geojson.validation import is_valid
+# import App.libs.geojson.factory as factory
+#
+# __all__ = ([dump, dumps, load, loads, GeoJSONEncoder] +
+#            [coords, map_coords] +
+#            [Point, LineString, Polygon] +
+#            [MultiLineString, MultiPoint, MultiPolygon] +
+#            [GeometryCollection] +
+#            [Feature, FeatureCollection] +
+#            [GeoJSON] +
+#            [is_valid],
+#            [factory])
